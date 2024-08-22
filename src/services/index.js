e1e09ffaf86ef27d12eda0bf23dd1f1aa4ea805a
Rafael-PC <rafaelreinertd@hotmail.com>
2024-08-22 02:17:31 -0300

    feat: services

---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+export * as precoService from "./precoService.js";
+export * as veiculoService from "./veiculoService.js";
+export * as utils from "./utils.js";
--- src/services/utils.js
+++ src/services/utils.js
@@ -1,1 +1,54 @@
-const apiUrl = "https://localhost:7217";
+export const diasSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"];
+export const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+export function textoRelogio() {
+  const data = new Date();
+
+  return `${diasSemana[data.getDay()]},
+      ${data.getDate()} de ${meses[data.getMonth()]} de ${data.getFullYear()},
+      ${data.getHours()}:${data.getMinutes().toString().padStart(2, 0)}:${data.getSeconds().toString().padStart(2, 0)}`;
+}
+
+export function formatarDataTabela(data) {
+  return (
+    data.getDate().toString().padStart(2, 0) + "/" +
+    data.getMonth().toString().padStart(2, 0) + "/" +
+    data.getFullYear().toString().padStart(4, 0) + " " +
+    data.getHours() + ":" +
+    data.getMinutes().toString().padStart(2, 0) + ":" +
+    data.getSeconds().toString().padStart(2, 0)
+  );
+}
+
+export function formatarDataForm(data) {
+  const arr = data.toLocaleString().split(",");
+
+  let dataSemHora = arr[0].split("/");
+  dataSemHora[2].padStart(4, 0); //transforma ano 35 em 0035 ISO
+  dataSemHora = dataSemHora.reverse().join("-");
+
+  const hora = arr[1].trim();
+
+  return [dataSemHora, hora]
+}
+
+export function stringParaData(str) {
+  if (str != null) return localizarData(new Date(str));
+  else return null;
+}
+
+function localizarData(data) {
+  const horaAtual = new Date();
+  const novaData = new Date(data)
+
+  const difHora = novaData.getHours() - (horaAtual.getTimezoneOffset() / 60);
+  const difMin = novaData.getMinutes() - (horaAtual.getTimezoneOffset() % 60);
+
+  novaData.setHours(difHora, difMin);
+
+  return novaData;
+}
+
+export function formatarIntParaValorTabela(valor) {
+  return (valor / 100).toFixed(2).replace(".", ",");
+}
